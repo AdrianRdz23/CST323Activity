f4f1a6c273c076997df14a545d03d929b6b5a9dc
Adrian Rodriguez-Aguilar <46502770+AdrianRdz23@users.noreply.github.com>
2022-01-27 20:58:46 -0700

    import changes

--- CSTActivity1/app/Http/Controllers/AdminController.php
+++ CSTActivity1/app/Http/Controllers/AdminController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\services\business\SecurityService;
+use App\Services\Business\SecurityService;
 
 class AdminController extends Controller
 {
